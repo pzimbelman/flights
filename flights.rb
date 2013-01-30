d5f13195a2986f058ca8bbe6d412021f25ee413a
Pete Zimbelman <pzimbelman@gmail.com>
2013-01-29 21:19:24 -0500

    add view and actually render results

--- flights.rb
+++ flights.rb
@@ -1,2 +1,18 @@
 require 'sinatra'
-require 'flight_schedule'
+require_relative 'flight_schedule'
+
+get '/' do
+  "This is a little app for Alison!"
+end
+
+get '/arriving_to/:airport_code' do
+  @json = FlightSchedule.new(ENV['FLIGHT_APP_ID'], ENV['FLIGHT_APP_KEY']).flights_direct_to(params[:airport_code])
+  @flights = @json["flights"].sort_by { |x| x["departureAirportFsCode"] }
+  haml :index
+end
+
+get '/departing_from/:airport_code' do
+  @json = FlightSchedule.new(ENV['FLIGHT_APP_ID'], ENV['FLIGHT_APP_KEY']).flights_direct_from(params[:airport_code])
+  @flights = @json["flights"].sort_by { |x| x["departureAirportFsCode"] }
+  haml :index
+end
